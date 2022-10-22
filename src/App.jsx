d9a62c2b2cdfb6dbc16754894307b66336f57bf9
jose8991 <joseluisgm1998@gmail.com>
2022-10-22 11:01:04 -0500

    se creo el primer componente

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CardComponent from './component/cardComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="row">
+        <CardComponent/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
