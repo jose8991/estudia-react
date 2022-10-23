dd67c03ba91f2f64181b33fe4ac04f6f85011d69
jose8991 <joseluisgm1998@gmail.com>
2022-10-23 11:23:53 -0500

    logica de la molecucla separada del orgaismo

--- src/moleculas/CardComponent.jsx
+++ src/moleculas/CardComponent.jsx
@@ -1,13 +1,20 @@
-function CardComponent({title, img}){
-    return (
-        <div className="card col-4">
-            <img className="card-img-top" src={img} alt="Card image cap"/>
-            <div className="card-body">
-                <h5 className="card-title">{}</h5>
-                <p className="card-text">{title}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    )
+function CardComponent({ title, img, AgregarAlCarrito }) {
+// importamos nuestras variables de estado 
+  return (
+    <div className="card col-4">
+
+      <img className="card-img-top" src={img} alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">{}</h5>
+        <p className="card-text">{title}</p>
+        <button
+          className="btn btn-primary"
+          onClick={(event) => AgregarAlCarrito(event, "toto")}
+        >
+   
+        </button>
+      </div>
+    </div>
+  );
 }
 export default CardComponent;
--- src/organismos/ProductsContainer.jsx
+++ src/organismos/ProductsContainer.jsx
@@ -1,15 +1,31 @@
+//impostamos useState para poder alterar o estado do componente
 import CardComponent from "../moleculas/CardComponent";
+import React, { useState } from "react";
 
-function ProductsContainer(){
-    return (
-        <div className="container">
-            <div className="row">
-            <CardComponent title={'hola mundo'} img={'https://www.xtrafondos.com/wallpapers/paisaje-digital-en-atardecer-5846.jpg'}/>
+function ProductsContainer() {
+  const [carrito, setCarrito] = useState([]);
+  const  AgregarAlCarrito= (event, parametro) => {
+    carrito.push(parametro);
+    setCarrito([...carrito]);
+    console.log(carrito);
+  }
+  return (
+    <div className="container">
+            {carrito.length }
+      <div className="row">
+    
+        <CardComponent
+          title={"hola mundo"}
+          img={
+            "https://www.xtrafondos.com/wallpapers/paisaje-digital-en-atardecer-5846.jpg"
+          }
+         AgregarAlCarrito={AgregarAlCarrito}
+        />
+        {/* <CardComponent/>
             <CardComponent/>
-            <CardComponent/>
-            <CardComponent/>
-            </div>
-        </div>
-    )
+            <CardComponent/> */}
+      </div>
+    </div>
+  );
 }
 export default ProductsContainer;
